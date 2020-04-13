c724bfbea293f4ca141554ae3fc738d7f8743102
OlesiaGuk <olesia.v.guk@yandex.ru>
2020-04-13 13:36:19 +0700

    [HelloWorld Servlet]

---
+++ task1_helloWorldServlet/src/ru/serverweb/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+package ru.serverweb;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class HelloWorldServlet extends HttpServlet {
+    private static final long serialVersionUID = 2891252518472296846L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getOutputStream().write("Hello from HelloWorldServlet".getBytes(StandardCharsets.UTF_8));
+        resp.getOutputStream().flush();
+        resp.getOutputStream().close();
+    }
+}
